3e568986e9d06b67926707078d8db7f172a85d2e
Julian Jandeleit <54635597+julianjandeleit@users.noreply.github.com>
2023-03-25 13:20:30 +0100

    flet datacontrol

---
+++ flet_main.py
@@ -0,0 +1,187 @@
+#!/usr/bin/env python
+import flet as ft
+import pandas as pd
+#import uuid
+
+
+def load_csv(path: str):
+    return pd.read_csv(path)
+
+# def df_to_view(df: pd.DataFrame):
+#    columns = [ft.DataColumn(ft.Text(name)) for name in df.columns ]
+
+
+df = load_csv("data/table_out.csv")
+df = df.rename(columns={"0":"A"})
+dfs = [df]
+
+class DataControl(ft.UserControl):
+    def __init__(self, data):
+        super().__init__()
+        self._data = data
+        self._widget = ft.Container()
+        
+        
+    def get_data(self):
+        return self._data
+        
+    def update_data(self, data):
+        self._data = data
+        self.update()
+        
+    def build_widget(self) -> ft.UserControl:
+        pass
+        
+    def _build_widget(self):
+        widget = self.build_widget()
+        self._widget.content = widget
+        
+    def build(self):
+        self._build_widget()
+        return self._widget
+        
+    def update(self):
+        self._build_widget()
+        super().update()
+
+class Test2(DataControl):
+    def build_widget(self) -> ft.UserControl:
+        text = ft.Text(self.get_data())
+        button = ft.TextButton("change", on_click=lambda e: self.update_data("new value"))
+        children = [text]
+        if self.data != "new value":
+            children += [button]
+        return ft.Row(controls=children)
+
+class Test(ft.UserControl):
+    def __init__(self, data):
+        print("init")
+        super().__init__()
+        self.data = data
+        self.widget = ft.Container()
+        
+    def build_widget(self):
+        self.text = ft.Text(self.data)
+        self.button = ft.TextButton("change", on_click=self.handle_click)
+        children = [self.text]
+        if self.data != "new value":
+            children += [self.button]
+        self.widget.content = ft.Row(controls=children)
+        #if self.data == "new value":
+        #    self.button = ft.Container(padding=10, bgcolor="red")
+        
+    def build(self):
+        print("build")
+        self.build_widget()
+        return self.widget
+        
+    def handle_click(self, e):
+        print("handle")
+        self.data = "new value"
+        self.update()
+        
+    def update(self):
+        print("update")
+        self.build_widget()
+        super().update()
+
+class CustomCell(ft.UserControl):
+    def __init__(self, value, row, col, parent):
+        super().__init__()
+        self.value = value
+        self.row = row
+        self.col = col
+        self.parent = parent
+
+    def build(self):
+        return ft.Text(self.value)
+
+    def open_dlg(self):
+        global df
+        tmp = list(df.loc[0])
+        tmp[0] = "test"
+        df.loc[0] = tmp
+        #df.rename(columns={"A": "Test"},inplace=True)
+        #print(self.df.columns)
+        dlg = ft.AlertDialog(title=ft.Text(
+            f"{self.value}"), content=ft.Text(f"({self.row}, {self.col})"))
+        self.page.dialog = dlg
+        dlg.open = True
+        self.page.update()
+
+
+class EditableDataView(ft.UserControl):
+    def __init__(self, data: pd.DataFrame):
+        super().__init__()
+        #print("create")
+        self.df = data
+        self.build_rows()
+        self.test = False
+     
+    def build_rows(self):
+        rows = []
+        for r in range(self.df.shape[0]):
+            cells = []
+            for c in range(self.df.shape[1]):
+                cells.append(ft.DataCell(
+                    CustomCell(self.df.iloc[r, c], r, c, self),
+                    show_edit_icon=False,
+                    on_tap=lambda e: e.control._DataCell__content.open_dlg()
+                ))
+            row = ft.DataRow(cells=cells)
+            rows.append(row)
+        self.rows = rows
+
+    def build(self):
+        #print(f"{df.columns}")
+        self.build_rows()
+        rws = self.rows
+        if self.test:
+            rws = []
+        self.test = True
+        return ft.DataTable(
+            columns=[ft.DataColumn(ft.Text(c)) for c in df.columns],
+            rows=rws,
+        )
+ 
+def main(page: ft.Page):
+
+
+    page.scroll = "ALWAYS"
+
+    t = ft.Text(value="Hello, world", color="green")
+    page.controls.append(t)
+
+    tt = ft.Text("A")
+
+    page.add(ft.Row(controls=[
+        tt,
+        ft.Text("B"),
+        ft.Text("C"),
+    ]
+    ))
+    
+    page.add(Test2("old data"))
+    page.add(Test2("older data"))
+
+    ev = ft.Ref[EditableDataView(df)]()
+
+    def button_clicked(e):
+        page.add(ft.Text(f"Clicked! {df.columns}\n{ev.df}"))
+        ev.current.build_rows()
+        ev.current.build()
+        ev.current.df = df
+        tt.value = "ASDF"
+        page.update()
+
+    page.add(ft.ElevatedButton(
+        text="Click Me",
+        on_click=button_clicked
+    ))
+
+    #page.add(ev)
+
+    page.update()
+
+
+ft.app(target=main)
---
+++ main.py
@@ -0,0 +1,28 @@
+#%%
+import tabula
+import pandas as pd
+import pdftotext
+path_pdf = "/home/julian/Downloads/Schwalbe Katalog 2022.pdf"
+#%%
+dfs = tabula.read_pdf(path_pdf,pages=[22,23],pandas_options={"header":None})
+len(dfs)
+first :pd.DataFrame = dfs[0]
+first.to_csv("data/table_out.csv",index=None)
+#tabula.convert_into(path_pdf, "output.csv", output_format="csv", pages="all")
+# %% Extract raw text (not that useful for us)
+
+#with open(path_pdf, "rb") as f:
+#    pdf = pdftotext.PDF(f)
+    
+#print(len(pdf))
+
+# Iterate over all the pages
+#for page in pdf:
+#    print(page)
+
+# Read some individual pages
+#print(pdf[0])
+#print(pdf[1])
+#with open("output.txt", "w") as text_file:
+#    text_file.write(pdf[22])
+# %%
